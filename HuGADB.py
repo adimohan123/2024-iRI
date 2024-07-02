942c5154568554a4aae2c8a1ea7c9bf3236f6338
adi <awesomeadi2007@gmail.com>
2024-07-02 16:08:15 -0400

    bugs fixed from coding session

---
+++ HuGADB.py
@@ -0,0 +1,6 @@
+
+function data = loadDataFromFile(filename)
+% loadDataFromFile - Load data from HuGaDB text file.
+%   filename - path to HuGaDB text file
+    data = dlmread(filename,'\t', 4,0);
+end
--- webscraper.py
+++ webscraper.py
@@ -1,20 +1,57 @@
+
 from bs4 import BeautifulSoup
 import requests
 
-response = requests.get('https://ninapro.hevs.ch/instructions/DB1.html')
-webpage = response.text
-soup = BeautifulSoup(webpage, 'html.parser')
-soup.find_all('a')
-urls = []
-for link in soup.find_all('a'):
-    s = link.get('href')
-    type(s)
-    if(s.endswith('.zip')):
-       s = s[3:]
-       hold =  'https://ninapro.hevs.ch/'  + s
-       urls.append(hold)
+fnout ='' # making this a global variable
+def get_files():
+    #files = ['DB1', 'DB2', 'DB4','DB7']
 
-print(urls)
+    # loop to make urls
+    # loop over urls
+
+    response = requests.get('https://ninapro.hevs.ch/instructions/DB1.html')
+    webpage = response.text
+    soup = BeautifulSoup(webpage, 'html.parser')
+    soup.find_all('a')
+    urls = []
+    for link in soup.find_all('a'):
+        s = link.get('href')
+        type(s)
+        if(s.endswith('.zip')):
+            s = s[3:]
+            hold =  'https://ninapro.hevs.ch/'  + s
+            urls.append(hold)
+
+    for url in urls:
+        # https://ninapro.hevs.ch/files/DB1/Preprocessed/s1.zip
+        print(url)
+        r = requests.get(url)
+        urlparts = url.split('/')
+        fnout = 'C://mydata/' + urlparts[-2] + '/' + urlparts[-1]
+        with open(fnout, 'wb') as f:
+            f.write(r.content)
+
+def unzip():
+    import zipfile
+    with zipfile.ZipFile(fnout, 'r') as zip_ref:
+        zip_ref.extractall() # new name with .mat
+
+def translate():
+    from scipy.io import loadmat
+    import pandas as pd
+    mat = loadmat('filename') # .mat filename
+    mat = {k :v for k, v in mat.items() if k[0] != '_'}
+    data = pd.DataFrame({k: pd.Series(v[0]) for k, v in mat.items()})
+    data.to_csv('') # create csv filename
+
+def preprocessing():
+    # load and process csv
+    pass
+
+def main():
+    # get_files()
 
 
 
+if __name__ == '__main__':
+    main()
