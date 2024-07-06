d1ba021fc12713fa0074f9443f9d40a30e3cc448
adi <awesomeadi2007@gmail.com>
2024-07-06 16:24:15 -0400

    + added data validator and downloaded completely dataset 4

--- DataTester.py
+++ DataTester.py
@@ -1,19 +1,20 @@
 import pandas as pd
 
-# Replace 'your_file.csv' with the path to your CSV file
-csv_file = # I will need code to cycle through this
+
+# Need help with cycling through files
+csv_file =
 
 # Read the CSV file into a DataFrame
 df = pd.read_csv(csv_file)
 
-# Initialize a dictionary to store counts of data points for each column
+
 data_points_count = {}
 
-# Iterate over the specified columns (emg[0] to emg[9])
+
 for i in range(10):
     column_name = f'emg{i}'
     if column_name in df.columns:
-        # Count the number of non-null data points in the column
+
         count = df[column_name].count()
         data_points_count[column_name] = count
     else:
