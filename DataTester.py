cd4d3223534d2865936379e2c97775f350e3e53c
adi <awesomeadi2007@gmail.com>
2024-07-08 21:25:33 -0400

    Played around with pandas and pyplot

--- DataTester.py
+++ DataTester.py
@@ -1,25 +1,22 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 # Need help with cycling through files
-csv_file =
+df = pd.read_csv("C:\\Users\\Aweso\\Downloads\\The folder\\Data\\DB1\\S9_A1_E2.csv")
+#1218 is the # of data rows corresponding to the 3 second of rest at the begining
+columns = []
+for x in range(10):
+    columns.append(f'emg{x}')
+time = list(range(1218))
 
-# Read the CSV file into a DataFrame
-df = pd.read_csv(csv_file)
+columns.append("restimulus")
+new_df = df[columns]
+first_150 = df['emg6'].head(1218)
+
+print(new_df.corr())
+print(new_df['emg6'].max())
+plt.plot(time,first_150)
+plt.show()
 
 
-data_points_count = {}
-
-
-for i in range(10):
-    column_name = f'emg{i}'
-    if column_name in df.columns:
-
-        count = df[column_name].count()
-        data_points_count[column_name] = count
-    else:
-        data_points_count[column_name] = 0
-
-# Print the results
-for column, count in data_points_count.items():
-    print(f'{column}: {count} data points')
