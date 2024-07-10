f5dfbd1dfe0ee82dd9cdb41cb116a3060a2ab456
adi <awesomeadi2007@gmail.com>
2024-07-09 22:35:30 -0400

    Trouble loading dataset

--- CustomCNN.py
+++ CustomCNN.py
@@ -1,30 +1,39 @@
-import tensorflow as tf
+import sys
 
+# Add the project directory to the Python path
+sys.path = 'C:\\Users\\Aweso\\Downloads\\The folder\\Data\\PreProc'
+
+# Verify the path was added correctly
+print("Python path:")
+print(sys.path)
+
+import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
+import tensorflow_datasets as tfds
 
-(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+# Import and register the custom dataset
+try:
+    import PreProc.db1  # Adjust this to match your actual module structure
+    print("Custom dataset module imported successfully.")
+except ImportError as e:
+    print("Error importing custom dataset module:", e)
 
-# Normalize pixel values to be between 0 and 1
-train_images, test_images = train_images / 255.0, test_images / 255.0
+# Load the custom dataset
+try:
+    ds = tfds.load('db1')  # Replace 'my_dataset' with your actual dataset name
+    print("Custom dataset loaded successfully.")
+    print(ds)
+except Exception as e:
+    print("Error loading custom dataset:", e)
 
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-               'dog', 'frog', 'horse', 'ship', 'truck']
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i])
-    # The CIFAR labels happen to be arrays,
-    # which is why you need the extra index
-    plt.xlabel(class_names[train_labels[i][0]])
-plt.show()
 
+
+
+'''
 model = models.Sequential()
-model.add(layers.Conv2D(32,(3,3),activation='relu'))
+model.add(layers.Conv1D(32,3,activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
@@ -53,3 +62,4 @@ plt.legend(loc='lower right')
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
 print(test_acc)
+'''
--- DataTester.py
+++ DataTester.py
@@ -1,22 +1,31 @@
 import pandas as pd
+import os
 import matplotlib.pyplot as plt
 
+# Path to the base directory
+base_path = "C:\\Users\\Aweso\\Downloads\\The folder\\Data\\DB1"
 
-# Need help with cycling through files
-df = pd.read_csv("C:\\Users\\Aweso\\Downloads\\The folder\\Data\\DB1\\S9_A1_E2.csv")
-#1218 is the # of data rows corresponding to the 3 second of rest at the begining
-columns = []
-for x in range(10):
-    columns.append(f'emg{x}')
-time = list(range(1218))
+# Creating the columns we want to transfer
+columns = [f'emg{x}' for x in range(10)]
+columns.extend(["restimulus", "rerepetition","exercise"])
 
-columns.append("restimulus")
-new_df = df[columns]
-first_150 = df['emg6'].head(1218)
-
-print(new_df.corr())
-print(new_df['emg6'].max())
-plt.plot(time,first_150)
-plt.show()
+# Looping through all the files in DB1 and copying the subset to a new directory
+for root, dirs, files in os.walk(base_path):
+    for file in files:
+        if file.endswith(".csv"):
+            full_file_path = os.path.join(root, file)
+            new_file_path = full_file_path.replace("DB1", "PreProc\\db1")
 
 
+            print(new_file_path)
+            df = pd.read_csv(full_file_path)
+
+            new_df = df[columns]
+            new_df.to_csv(new_file_path, index=False)
+
+
+
+# print(new_df.corr())
+# print(new_df['emg6'].max())
+# plt.plot(time, new_df['emg0'][:150])  # Adjust 'emg0' and 150 as needed
+# plt.show()
